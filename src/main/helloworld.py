e4d8423942aad68e808f0ce8975712a166b9f4dd
Kalpesh Kocharekar <kalpeshk71@gmail.com>
2020-08-09 19:37:01 +0530

    Adding python basics Hello world and data types code

--- src/main/helloworld.py
+++ src/main/helloworld.py
@@ -43,11 +43,29 @@ the end.
 
 The plus (+) sign is the string concatenation operator and the asterisk (*) is the repetition operator. For example −'''
 
-str = 'Hello World!'
+hello_str = 'Hello World!'
 
-print(str)  # Prints complete string
-print(str[0])  # Prints first character of the string i.e. H
-print(str[2:5])  # Prints characters starting from 3rd to 5th i.e. llo
-print(str[2:])  # Prints string starting from 3rd character i.e. llo World!
-print(str * 2)  # Prints string two times i.e. Hello World!Hello World!
-print(str + "TEST")  # Prints concatenated string i.e. Hello World!TEST
+print(hello_str)  # Prints complete string
+print(hello_str[0])  # Prints first character of the string i.e. H
+print(hello_str[2:5])  # Prints characters starting from 3rd to 5th i.e. llo
+print(hello_str[2:])  # Prints string starting from 3rd character i.e. llo World!
+print(hello_str * 2)  # Prints string two times i.e. Hello World!Hello World!
+print(hello_str + "TEST")  # Prints concatenated string i.e. Hello World!TEST
+
+print('''Python Lists Lists are the most versatile of Python's compound data types. A list contains items separated by 
+commas and enclosed within square brackets ([]). To some extent, lists are similar to arrays in C. One difference 
+between them is that all the items belonging to a list can be of different data type. 
+
+The values stored in a list can be accessed using the slice operator ([ ] and [:]) with indexes starting at 0 in the 
+beginning of the list and working their way to end -1. The plus (+) sign is the list concatenation operator, 
+and the asterisk (*) is the repetition operator. For example − ''')
+
+test_list = ['abcd', 786, 2.23, 'john', 70.2]
+tinylist = [123, 'john']
+
+print(test_list)  # Prints complete list
+print(test_list[0])  # Prints first element of the list
+print(test_list[1:3])  # Prints elements starting from 2nd till 3rd
+print(test_list[2:])  # Prints elements starting from 3rd element
+print(tinylist * 2)  # Prints list two times
+print(test_list + tinylist)  # Prints concatenated lists
